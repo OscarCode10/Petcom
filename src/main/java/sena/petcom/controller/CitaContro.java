49663deca174f90b024791a3ef94fb81ce065bef
deyby <125367981+CristianDas@users.noreply.github.com>
2023-11-10 09:53:38 -0500

    a

--- src/main/java/sena/petcom/controller/CitaContro.java
+++ src/main/java/sena/petcom/controller/CitaContro.java
@@ -1,6 +1,13 @@
 package sena.petcom.controller;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.List;
+import java.io.ByteArrayOutputStream;
 
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,6 +16,8 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.support.SessionStatus;
 
 import sena.petcom.model.Cita.Cita;
@@ -80,6 +89,37 @@ public class CitaContro {
         iCita.save(cita);
         status.setComplete();
         return "redirect:/listCita";
+    }
+}
+
+@Controller
+@RequestMapping("/reporteCitas")
+public class ReporteCitaController {
+
+    @GetMapping("/pdf")
+    @ResponseBody
+    public byte[] generarReportePDF() throws IOException {
+        // Crea un documento PDF
+        PDDocument document = new PDDocument();
+        PDPage page = new PDPage();
+        document.addPage(page);
+
+        // Contenido del informe
+        try (PDPageContentStream contentStream = new PDPageContentStream(document, page)) {
+            contentStream.setFont(PDType1Font.HELVETICA_BOLD, 12);
+            contentStream.beginText();
+            contentStream.newLineAtOffset(100, 700);
+            contentStream.showText("Informe de Citas");
+            // Agrega más contenido según tus necesidades
+            contentStream.endText();
+        }
+
+        // Convierte el documento a un array de bytes
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        document.save(byteArrayOutputStream);
+        document.close();
+
+        return byteArrayOutputStream.toByteArray();
     }
 }
 
