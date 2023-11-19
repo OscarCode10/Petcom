aed2bbf952000641403163c5c0f422d2178a9e53
andrew052 <125332755+andrew052@users.noreply.github.com>
2023-11-19 00:35:40 -0500

    vistas de reportes

--- src/main/java/sena/petcom/controller/HistoriaClinicaControlador.java
+++ src/main/java/sena/petcom/controller/HistoriaClinicaControlador.java
@@ -1,12 +1,17 @@
 package sena.petcom.controller;
 
+import org.apache.tomcat.util.file.ConfigurationSource.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import com.lowagie.text.List;
 
 import sena.petcom.model.HistoriaClinica.IHistoriaClinica;
 import sena.petcom.model.HistoriaClinica.HistoriaClinica;
@@ -43,5 +48,24 @@ public class HistoriaClinicaControlador {
         m.addAttribute("historias", iHistoriaClinica.findAll());
         return "historiaClinica/listarHistoria";
     }
+    
+    @GetMapping("/reporteHistoria")
+    public String reporteHistoria(){
+        return "historiaClinica/reporteHistoria";
+    }
+    
+    @GetMapping("/reporteHistoriaV/{idHistoriaClinica}")
+    public String reporteHistoriaV(@PathVariable Integer idHistoriaClinica,Model m){
+        HistoriaClinica historia=null;
+        if (idHistoriaClinica>0) {
+            historia=iHistoriaClinica.findOne(idHistoriaClinica);
+            m.addAttribute("historia", historia );
+            return "HistoriaClinica/reporteHistoria";
+        }
+        return "redirect:/listarHistoria";
+    }
+
+    
+
     
 }
