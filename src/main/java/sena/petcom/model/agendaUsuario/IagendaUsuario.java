c876e25ee5ec0c89231194854b92d4950bb47d28
OscarCode10 <oscaortiz10@gmail.com>
2023-11-22 00:00:41 -0500

    a.

--- src/main/java/sena/petcom/model/agendaUsuario/IagendaUsuario.java
+++
@@ -1,9 +0,0 @@
-package sena.petcom.model.AgendaUsuario;
-
-import java.util.List;
-
-public interface IAgendaUsuario {
-    public void save (AgendaUsuario agendaUsu);
-    public List<AgendaUsuario> findAll();
-    public AgendaUsuario findOne(Integer idAgendaUsuario);
-} 
