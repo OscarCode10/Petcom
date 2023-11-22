1e675a0fb644483d5faed465be3f6231de99c7ac
OscarCode10 <oscaortiz10@gmail.com>
2023-11-22 00:04:27 -0500

    Aplicacion inicia

---
+++ src/main/java/sena/petcom/model/AgendaUsuario/AgendaUsuarioDAO.java
@@ -0,0 +1,39 @@
+package sena.petcom.model.AgendaUsuario;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+
+@Repository
+public class AgendaUsuarioDAO implements IAgendaUsuario{
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Transactional()
+    @Override
+    public void save(AgendaUsuario agendaUsu) {
+        if (agendaUsu.getIdAgendaUsuario() != null && agendaUsu.getIdAgendaUsuario()>0){
+            em.merge(agendaUsu);
+        } else {
+            em.persist(agendaUsu);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    @Transactional
+    @Override
+    public List<AgendaUsuario> findAll() {
+        return em.createQuery("from AgendaUsuario").getResultList();
+    }
+
+    @Override
+    public AgendaUsuario findOne(Integer idAgendaUsuario) {
+        return em.find(AgendaUsuario.class, idAgendaUsuario);
+    }
+    
+}
---
+++ src/main/java/sena/petcom/model/AgendaUsuario/IAgendaUsuario.java
@@ -0,0 +1,9 @@
+package sena.petcom.model.AgendaUsuario;
+
+import java.util.List;
+
+public interface IAgendaUsuario {
+    public void save (AgendaUsuario agendaUsu);
+    public List<AgendaUsuario> findAll();
+    public AgendaUsuario findOne(Integer idAgendaUsuario);
+} 
